0079d50b577c72eeba73edca0c86c27a9973479b
Rutvik Gondekar <gondekarrutvik@gmail.com>
2022-05-20 13:56:23 +0530

    Day 14 1/2

--- 06-Components/script.js
+++ 06-Components/script.js
@@ -5,3 +5,24 @@ btns.forEach((btn) =>
     this.closest(".item").classList.toggle("open");
   })
 );
+
+// const carouselBtnLeft = document.querySelector(".btn--left");
+// const carouselBtnRight = document.querySelector(".btn--right");
+// const dots = Array.from(document.querySelectorAll(".dot"));
+// let currentDot = 0;
+// const numDots = 4;
+
+// carouselBtnLeft.addEventListener("click", function (e) {
+//   dots.forEach((dot) => dot.classList.remove("dot--fill"));
+//   if (currentDot === 0) {
+//     currentDot = numDots;
+//     dots
+//       .find((dot) => dot.dataset.num === currentDot)
+//       .classList.add("dot--fill");
+//   } else {
+//     currentDot--;
+//     dots
+//       .find((dot) => dot.dataset.num === currentDot)
+//       .classList.add("dot--fill");
+//   }
+// });
